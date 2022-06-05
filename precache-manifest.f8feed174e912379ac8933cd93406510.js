3a8895f3b2273fdbade6c1881af88fed929dde1e
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-06-05 21:16:08 +0530

    update: v2.2
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

---
+++ precache-manifest.f8feed174e912379ac8933cd93406510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fae204905991358ea8cc",
+    "url": "/static/css/main.5f67f977.chunk.css"
+  },
+  {
+    "revision": "fae204905991358ea8cc",
+    "url": "/static/js/main.fae20490.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "341c9c61daa0b7386d2a",
+    "url": "/static/js/2.341c9c61.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "a537a2d511bae9f99f4c65e255786da9",
+    "url": "/index.html"
+  }
+];
