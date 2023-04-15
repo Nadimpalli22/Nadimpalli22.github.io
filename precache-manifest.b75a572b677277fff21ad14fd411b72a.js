00ba0bf2a2db6a9497e0ccbfc9f67e902af22fb1
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-04-15 16:16:39 +0530

    init fix 2

---
+++ precache-manifest.b75a572b677277fff21ad14fd411b72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84ebac4a0629deb2ac70",
+    "url": "/static/css/main.5715e3d9.chunk.css"
+  },
+  {
+    "revision": "84ebac4a0629deb2ac70",
+    "url": "/static/js/main.84ebac4a.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "b23fe9c5da5a96c04cd5",
+    "url": "/static/js/2.b23fe9c5.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "cbb93a0ab53511368e02315f7fd04655",
+    "url": "/index.html"
+  }
+];
